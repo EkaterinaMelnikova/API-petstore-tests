f3cb16e609aa1ac0fac3986c9f3dfd34f92a16be
EkaterinaMelnikova <katkimo@gmail.com>
2025-12-24 10:40:06 +0300

    добавлена ветка allure, теперь есть отчеты в allure.createUserLombokStepsTest - пример с оберткой в step-ы

--- src/test/java/tests/PetstoreExtendedTests.java
+++ src/test/java/tests/PetstoreExtendedTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import model.lombok.CreateUserResponseLombokModel;
 import model.pojo.CreateUserBodyLombokModel;
 import model.pojo.CreateUserResponseModel;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,8 +19,7 @@ public class PetstoreExtendedTests {
 
     @Test
     void createUserLombokTest() {
-        //String data =  "[{\"id\":777,\"username\":\"Kimo\",\"firstName\":\"Ekaterina\",\"lastName\":\"Kimo\",\"email\":\"12345@gmail.com\",\"password\":\"123\",\"phone\":\"12345678\",\"userStatus\":10}]";
-        CreateUserBodyLombokModel body= new CreateUserBodyLombokModel();
+        CreateUserBodyLombokModel body = new CreateUserBodyLombokModel();
         body.setId(777);
         body.setUsername("Kimo");
         body.setFirstName("Ekaterina");
@@ -30,6 +31,7 @@ public class PetstoreExtendedTests {
 
 
         CreateUserResponseLombokModel response = given()
+                .filter(new AllureRestAssured())
                 .log().uri()
                 .contentType(JSON)
                 .body(List.of(body))   // ← квадратные скобки
@@ -46,6 +48,40 @@ public class PetstoreExtendedTests {
         assertThat(response.getMessage()).isEqualTo("ok");
         assertThat(response.getType()).isEqualTo("unknown");
 
+    }
 
+    @Test
+    void createUserLombokStepsTest() {
+        CreateUserBodyLombokModel body = new CreateUserBodyLombokModel();
+        body.setId(777);
+        body.setUsername("Kimo");
+        body.setFirstName("Ekaterina");
+        body.setLastName("Kimo");
+        body.setEmail("12345@gmail.com");
+        body.setPassword("123");
+        body.setPhone("12345678");
+        body.setUserStatus(10);
+
+
+        CreateUserResponseLombokModel response =
+                step("Get user data", () ->
+                        given()
+                                .filter(new AllureRestAssured())
+                                .log().uri()
+                                .contentType(JSON)
+                                .body(List.of(body))   // ← квадратные скобки
+                                .when()
+                                .post("https://petstore.swagger.io/v2/user/createWithList")
+                                .then()
+                                .log().status()
+                                .log().body()
+                                .statusCode(200)
+                                .extract().as(CreateUserResponseLombokModel.class));
+
+        step("Verify response", () -> {
+            assertThat(response.getCode()).isEqualTo(200);
+            assertThat(response.getMessage()).isEqualTo("ok");
+            assertThat(response.getType()).isEqualTo("unknown");
+        });
     }
 }
